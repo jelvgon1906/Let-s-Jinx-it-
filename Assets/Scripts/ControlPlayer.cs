f8dc3341c4b58139b62daa1a47428057e7abe426
jelvgon1906 <jelvgon1906@g.educaand.es>
2022-11-28 21:20:13 +0100

    Weapon Change
    You can change between 3 weapons

--- Assets/Scripts/ControlPlayer.cs
+++ Assets/Scripts/ControlPlayer.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class ControlPlayer : MonoBehaviour
 { [Header("Movement")]
@@ -17,21 +18,23 @@ public class ControlPlayer : MonoBehaviour
     float rotationX;
     [SerializeField]private Camera CameraPlayer;
     private Animator animator;
-    [SerializeField] private Zap zap;
+    [SerializeField] private ZapController zap;
+    [SerializeField] private FishBonesController fishBones;
     [SerializeField] private GameObject handR;
-
-    
-
-    
+    [SerializeField] private GameObject EnergyBar;
+    GameObject targetWeapon;
+    GameObject ZapWeapon, FishBonesWeapon, BombWeapon;
     Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        FishBonesWeapon = GameObject.Find("/Player/CameraPlayer/handR/FishBones");
+        ZapWeapon = GameObject.Find("/Player/CameraPlayer/handR/Zap");
+        BombWeapon = GameObject.Find("/Player/CameraPlayer/handR/Bomb");
         animator = GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody>();
         CameraPlayer = Camera.main;
-        zap = FindObjectOfType(typeof(Zap)) as Zap;
     }
 
     // Update is called once per frame
@@ -43,26 +46,55 @@ public class ControlPlayer : MonoBehaviour
 
         if (Input.GetButton("Fire1"))
         {
-            
-            if (zap.canShoot())
+            /*if (zap.canShoot() && targetWeapon == FishBonesWeapon)*/
+            if (zap.canShoot() && targetWeapon == ZapWeapon)
             {
                 zap.Shoot();
             }
         }
 
-        if (Input.GetButton("Fire2"))
+        if (Input.GetButton("Fire2") && targetWeapon == ZapWeapon)
         {
-            zap.transform.localPosition = new Vector3(-0.170000002f, -0.188999996f, 0.238000005f);
+            zap = FindObjectOfType(typeof(ZapController)) as ZapController;
+            handR.transform.localPosition = new Vector3(-0.211999997f, -0.0149999997f, 0f);
 
         }
-        else zap.transform.localPosition = new Vector3(0.0399000011f, -0.188999996f, 0.238000005f);
+        else if(Input.GetButtonUp("Fire2") && targetWeapon == ZapWeapon) handR.transform.localPosition = Vector3.zero;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            handR.transform.localPosition = new Vector3(0.107000001f, 0f, 0.100000001f);
+            targetWeapon = FishBonesWeapon;
+            ChangeWeapon(/*FishBonesWeapon*/);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            targetWeapon = ZapWeapon;
+            handR.transform.localPosition = Vector3.zero;
+            ChangeWeapon(/*ZapWeapon*/);
+            EnergyBar.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            targetWeapon = BombWeapon;
+            handR.transform.localPosition = Vector3.zero;
+            ChangeWeapon(/*BombWeapon*/);
+        }
 
         Cursor.lockState = CursorLockMode.Locked;
 
 
     }
 
-    
+    private void ChangeWeapon(/*GameObject targetWeapon*/)
+    {
+        FishBonesWeapon.SetActive(false);
+        ZapWeapon.SetActive(false);
+        BombWeapon.SetActive(false);
+        EnergyBar.SetActive(false);
+
+        targetWeapon.SetActive(true);
+    }
 
     private void Jump()
     {
